232b5548416dda07929c667ed058a9cac5852f68
JohanK82 <101264418+JohanK82@users.noreply.github.com>
2026-03-04 15:16:29 +0100

    Remove unnecessary blank lines in app.py

--- app/files/app/app.py
+++ app/files/app/app.py
@@ -25,12 +25,10 @@ os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 IMG_EXTENSIONS = {".svg", ".png", ".jpg", ".jpeg", ".gif"}
 
-
 def is_img_file(fname):
     """Check if filename has a valid image extension."""
     _, extension = splitext(fname)
     return extension.lower() in IMG_EXTENSIONS
-
 
 def get_images():
     """
@@ -61,12 +59,10 @@ def get_images():
 
     return images
 
-
 # Define route for the index (home) page, accepting only GET method
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", images=get_images())
-
 
 # Define route with accepted methods GET and POST
 @app.route("/upload", methods=["GET", "POST"])
@@ -94,18 +90,15 @@ def upload_file():
     # If the method of the request is GET, show an upload form to the user
     return render_template("message.html", message="Upload your photos.")
 
-
 # Define route for the preview page that accepts a dynamic 'name' parameter in the URL
 @app.route("/preview/<imgname>")
 def preview(imgname):
     return render_template("preview.html", name=imgname)
 
-
 # Assuming 'preview' end-point exists to show the uploaded image to the user
 @app.route("/download/<imgname>")
 def download(imgname):
     return send_from_directory(app.config["UPLOAD_FOLDER"], imgname, as_attachment=True)
-
 
 # Define a route to serve static files. For example, images, CSS, JavaScript, etc.
 @app.route("/static/<name>")
@@ -114,7 +107,6 @@ def get_static(name):
 
 # Define route to delete an image
 @app.route("/delete/<imgname>", methods=["POST"])
-
 
 def delete_image(imgname):
     """
@@ -136,7 +128,6 @@ def delete_image(imgname):
     except OSError as err:
             print(f"Filesystem error: {err}", file=sys.stderr)
     return redirect("/")
-
 
 # To keep the application running
 if __name__ == "__main__":
