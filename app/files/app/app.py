3719ba2ab5376b777d2b0231c31ab614bddb8fa7
JohanK82 <101264418+JohanK82@users.noreply.github.com>
2026-03-04 15:01:23 +0100

    Add docstring to delete_image function
    Added a docstring to the delete_image function to explain its purpose.

--- app/files/app/app.py
+++ app/files/app/app.py
@@ -114,7 +114,6 @@ def get_static(name):
 
 # Define route to delete an image
 @app.route("/delete/<imgname>", methods=["POST"])
-
 def delete_image(imgname):
     """
     Delete an image from the filesystem.
