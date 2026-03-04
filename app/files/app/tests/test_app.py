e86d75c8a3cce554f40cf98127e1de3226a70bbd
JohanK82 <101264418+JohanK82@users.noreply.github.com>
2026-03-04 16:20:39 +0100

    Clean up test_app.py by removing a blank line
    Remove unnecessary blank line in test_app.py

--- app/files/app/tests/test_app.py
+++ app/files/app/tests/test_app.py
@@ -15,7 +15,6 @@ from app import app as flask_app
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
 
-
 @pytest.fixture
 def app():
     """Create Flask application for testing."""
